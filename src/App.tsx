24e0f224341f9b6250aa08ac0917015f25aedecc
moe-zbib <mohammad.h.zbib@gmail.com>
2024-05-10 21:16:02 +0300

    New Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Button from "./modules/core/components/Button";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Button>HI</Button>
+    </div>
+  );
 };
 
 export default App;
