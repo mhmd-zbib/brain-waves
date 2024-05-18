27e85da6f060fbcc4757ef5c64e7cb6cd6f0304a
moe-zbib <mohammad.h.zbib@gmail.com>
2024-05-18 16:03:34 +0300

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,33 @@
-import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
-// import Navbar from "./core/components/layout/Navbar/Navbar";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SidePanel } from "./core/components/layout/SidePanel";
 import QuestionDetails from "./features/Questions/QuestionDetails/QuestionDetails";
 import Questions from "./features/Questions/QuestionsList";
 import { Sidebar } from "./core/components/layout/Sidebar";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <div className="  grid gap-3  sm:grid-cols-12 container mx-auto  relative    ">
-        <Sidebar />
-        <div className=" sm:col-span-12 lg:col-span-9 xl:col-span-6   ">
-          <Outlet />
-        </div>
-        <SidePanel />
-      </div>
-    ),
-    errorElement: <div>oh</div>,
-    children: [
-      {
-        path: "q/home",
-        element: <Questions />,
-      },
-
-      {
-        path: "q/:major/:course/:id",
-        element: <QuestionDetails />,
-      },
-
-      {
-        path: "q/:major",
-      },
-      { path: "q/:major/:course", element: <Questions /> },
-    ],
-  },
-]);
-
 function App() {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <div className="grid gap-3 sm:grid-cols-12 container mx-auto relative">
+          <Sidebar />
+          <div className="sm:col-span-12 lg:col-span-9 xl:col-span-6">
+            <Routes>
+              <Route path="*" element={<Navigate to="q/home" />} />
+              <Route path="q/home" element={<Questions />} />
+              <Route
+                path="q/:major/:course/:id"
+                element={<QuestionDetails />}
+              />
+              <Route path="q/:major" element={<Questions />} />
+              <Route path="q/:major/:course" element={<Questions />} />
+            </Routes>
+          </div>
+          <SidePanel />
+        </div>
+      </BrowserRouter>
     </QueryClientProvider>
   );
 }
